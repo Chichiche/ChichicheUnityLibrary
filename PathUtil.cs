518ec88e134eb29fb77fbef179d13690c4493a0b
Chichiche <Chichiche@users.noreply.github.com>
2024-07-22 19:54:32 +0900

    自動保存を汎用化

---
+++ PathUtil.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Chichiche
+{
+    public static class PathUtil
+    {
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("Tools/" + nameof( OpenSaveDataDirectory ))]
+        public static void OpenSaveDataDirectory()
+        {
+            System.Diagnostics.Process.Start(Application.persistentDataPath);
+        }
+#endif
+
+        public static string GetSaveDataFilePath(string key)
+        {
+            return Path.Combine(Application.persistentDataPath, GetFileName(key));
+        }
+
+        public static string GetEmbeddedFilePath(string key)
+        {
+            return Path.Combine(Application.streamingAssetsPath, GetFileName(key));
+        }
+
+        static string GetFileName(string key)
+        {
+            return $"{key.Md5().Replace('/', '_')}.dat";
+        }
+    }
+}
---
+++ SaveDataRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using R3;
+using VContainer.Unity;
+
+namespace Chichiche
+{
+    public abstract class SaveDataRepository<T> : ILateTickable, IDisposable where T : new()
+    {
+        readonly VariableLengthSaveDataStream<T> _stream;
+        readonly T _data;
+        bool _isDirty;
+
+        protected SaveDataRepository()
+        {
+            var saveDataPath = PathUtil.GetSaveDataFilePath(typeof( T ).Name);
+            _stream = new VariableLengthSaveDataStream<T>(saveDataPath);
+            _data = _stream.Load();
+        }
+
+        public Observable<T> AsObservable()
+        {
+            return Observable.Return(_data);
+        }
+
+        public void Update<TValue>(TValue value, Action<T, TValue> update)
+        {
+            update(_data, value);
+            _isDirty = true;
+        }
+
+        public void LateTick()
+        {
+            if (! _isDirty) return;
+            _isDirty = false;
+            _stream.Save();
+        }
+
+        public void Dispose()
+        {
+            _stream?.Dispose();
+        }
+    }
+}
